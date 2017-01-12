938ec669a1d310fe903421c7377284ba0c5d013e
Breno Henrique de Lima Freitas <brenosc2@hotmail.com.br>
2017-01-12 02:10:21 +0000

    receiving messages. todo: manipulate messages

--- server.js
+++ server.js
@@ -2,11 +2,15 @@ var http = require('http');
 var path = require('path');
 
 var express = require('express');
-
 var router = express();
 var server = http.createServer(router);
 
+var bodyParser = require("body-parser");
+var request = require("request");
+
 router.use(express.static(path.resolve(__dirname, 'client')));
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({extended: false}));
 
 router.get('/webhook', (req, res) => {
   if(req.query['hub.mode'] === 'subscribe' && req.query['hub.verify_token'] === 'minhasenhaboladona' ){
@@ -17,6 +21,27 @@ router.get('/webhook', (req, res) => {
     res.sendStatus(403);
   }
 });
+
+router.post('/webhook', (req, res) => {
+  var data = req.body;
+  
+  if(data && data.object === 'page'){
+    data.entry.forEach((entry) => {
+      var pageId = entry.id;
+      var timeOfEvent = entry.time;
+      
+      entry.messaging.forEach((event) => {
+        if(event.message){
+          console.log(event.message);
+        }
+      });
+    });
+    
+    res.sendStatus(200);
+  }
+});
+
+
 
 server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
   var addr = server.address();
